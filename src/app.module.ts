f0d3ffb807d66f0678eefe663c3eab31d79cab80
MRDGH2821 <mihirrabade@gmail.com>
2022-01-28 17:02:08 +0530

    feat: add auth controllers & services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { AuthController } from './auth/controllers/auth.controller';
+import { AuthService } from './auth/services/auth.service';
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env.development',
-    })
+    }),
+    AuthModule
   ],
-  controllers: [],
-  providers: [],
+  controllers: [AuthController],
+  providers: [AuthService],
 })
 export class AppModule { }
